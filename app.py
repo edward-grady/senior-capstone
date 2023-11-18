e1a68054dcfac2aefdc0669c12c8b22bda5cb8cc
edward-grady <61165459+edward-grady@users.noreply.github.com>
2023-11-18 20:30:49 +0000

    added beginning of flask project webpage. app.py is the file from which the webpage will be run
    run
    pip install flask
    before trying to run program during dev
    
    go to
        https://flask.palletsprojects.com/en/3.0.x/installation/#python-version
    to find out how to run on local machine. must set up virtual environment if on local machine or use replit (or something similar)

---
+++ app.py
@@ -0,0 +1,37 @@
+'''must install flask environment before running scipt
+    run this in the shell:
+    
+    pip install flask
+
+
+    if running on local machine, follow instructions in this webpage:
+    https://flask.palletsprojects.com/en/3.0.x/installation/#python-version
+'''
+
+#import flask, only place flask import needed
+from flask import Flask
+
+app = Flask(__name__)
+
+
+#home page
+@app.route("/")
+def home_page():
+  return "This is the landing page for senior capstone"
+
+
+#stress-ng page websitename.com/stress_ng
+@app.route("/stress_ng")
+def stress_ng_page():
+  return "stress_ng"
+
+
+#load management page websitename.com/load_management
+@app.route("/load_management")
+def load_management_page():
+  return "load-management"
+
+
+#run app
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
