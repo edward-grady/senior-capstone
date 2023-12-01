417f8b0a0b7220a82a6d8110e5d092e3605de8cb
Aaron Terrazas <61167114+Aterrazas01@users.noreply.github.com>
2023-11-30 20:03:43 -0600

    Add files via upload
    App.py with stress_ng, home, and load management functionalities

--- app.py
+++ app.py
@@ -11,6 +11,7 @@
 
 #import flask, only place flask import needed
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_restx import Api, Resource, reqparse, fields
 import subprocess
 import logging
 import schedule
@@ -21,6 +22,7 @@ from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
+api = Api(app, doc='/swagger')
 
 app.secret_key = "SecretKey"
 
@@ -111,6 +113,115 @@ def home_page():
 @app.route("/stress_ng")
 def stress_ng_page():
   return render_template('stress_ng.html')
+
+# Stress-ng API routes
+output_model = api.model('Output', {'output': fields.String})
+
+common_parser = reqparse.RequestParser()
+common_parser.add_argument('io_workers',
+                           type=int,
+                           default=1,
+                           help='Number of I/O workers')
+common_parser.add_argument('stress_time',
+                           type=int,
+                           default=60,
+                           help='Stress time in seconds')
+
+
+def format_output(output):
+  return '<br>'.join(output.split('\n'))
+
+
+@api.route('/stress_cpu')
+class StressCpuResource(Resource):
+
+  @api.expect(common_parser)
+  @api.marshal_with(output_model)
+  def post(self):
+    args = common_parser.parse_args()
+    cpu_workers = args['io_workers']
+    stress_time = args['stress_time']
+    wsl_username = subprocess.check_output(
+        ['wsl', '/bin/bash', '-c', 'whoami']).strip().decode('utf-8')
+    cmd = f'wsl --user {wsl_username} /bin/bash -c "/usr/bin/stress-ng --cpu {cpu_workers} --timeout {stress_time}s --metrics-brief"'
+    print(f"Executing command: {cmd}")
+
+    try:
+      output = subprocess.check_output(cmd,
+                                       stderr=subprocess.STDOUT,
+                                       shell=True)
+      formatted_output = format_output(output.decode('utf-8'))
+      return {'output': formatted_output}
+    except subprocess.CalledProcessError as e:
+      return {'error': f"Error: {e.output.decode('utf-8')}"}
+
+
+@api.route('/stress_memory')
+class StressMemoryResource(Resource):
+
+  @api.expect(common_parser)
+  @api.marshal_with(output_model)
+  def post(self):
+    args = common_parser.parse_args()
+    memory_workers = args['memory_workers']
+    stress_time = args['stress_time']
+    wsl_username = subprocess.check_output(
+        ['wsl', '/bin/bash', '-c', 'whoami']).strip().decode('utf-8')
+    cmd = f'wsl --user {wsl_username} /bin/bash -c "/usr/bin/stress-ng --vm {memory_workers} --timeout {stress_time}s --metrics-brief"'
+    print(f"Executing command: {cmd}")
+
+    try:
+      output = subprocess.check_output(cmd,
+                                       stderr=subprocess.STDOUT,
+                                       shell=True)
+      formatted_output = format_output(output.decode('utf-8'))
+      return {'output': formatted_output}
+    except subprocess.CalledProcessError as e:
+      return {'error': f"Error: {e.output.decode('utf-8')}"}
+
+
+@api.route('/stress_io')
+class StressIOResource(Resource):
+
+  @api.expect(common_parser)
+  @api.marshal_with(output_model)
+  def post(self):
+    args = common_parser.parse_args()
+    io_workers = args['io_workers']
+    stress_time = args['stress_time']
+    wsl_username = subprocess.check_output(
+        ['wsl', '/bin/bash', '-c', 'whoami']).strip().decode('utf-8')
+    cmd = f'wsl --user {wsl_username} /bin/bash -c "/usr/bin/stress-ng --io {io_workers} --timeout {stress_time}s --metrics-brief"'
+    print(f"Executing command: {cmd}")
+
+    try:
+      output = subprocess.check_output(cmd,
+                                       stderr=subprocess.STDOUT,
+                                       shell=True)
+      formatted_output = format_output(output.decode('utf-8'))
+      return {'output': formatted_output}
+    except subprocess.CalledProcessError as e:
+      return {'error': f"Error: {e.output.decode('utf-8')}"}
+
+
+@app.route('/swagger')
+def swagger_ui():
+  return render_template('swaggerui.html')
+
+
+@app.route('/remote')
+def remote_ui():
+  # Add the logic for your remote route
+  return render_template('remote.html')
+
+
+# Output route
+@app.route('/output')
+def output():
+  output_data = request.args.get('output', '')
+  formatted_output = format_output(output_data)
+  return render_template('output.html', output=formatted_output)
+
 
 
 #load management page websitename.com/load_management
